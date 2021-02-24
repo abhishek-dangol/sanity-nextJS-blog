8586935c785d8029d403987a0eb22312e6f8a30e
Abhishek Dangol <abhi.dangol@gmail.com>
2021-02-24 15:54:55 -0600

    first commit

---
+++ components/CardItem.js
@@ -0,0 +1,35 @@
+import { Card } from "react-bootstrap";
+
+const CardItem = ({ title, subtitle, image, date, author }) => {
+  return (
+    <Card className={`fj-card`}>
+      <div className="card-body-wrapper">
+        <Card.Header className="d-flex flex-row">
+          <img
+            src={author.avatar}
+            className="rounded-circle mr-3"
+            height="50px"
+            width="50px"
+            alt="avatar"
+          />
+          <div>
+            <Card.Title className="font-weight-bold mb-1">
+              {author.name}
+            </Card.Title>
+            <Card.Text className="card-date">{date}</Card.Text>
+          </div>
+        </Card.Header>
+        <div className="view overlay">
+          <Card.Img src={image} alt="Card image cap" />
+        </div>
+        <Card.Body>
+          <Card.Title className="card-main-title">{title}</Card.Title>
+          <Card.Text>{subtitle}</Card.Text>
+        </Card.Body>
+      </div>
+      <a className="card-button">Read More</a>
+    </Card>
+  );
+};
+
+export default CardItem;
