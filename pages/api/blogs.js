f61440ecb4f8980baa101805e235c9cc365c57b5
Abhishek Dangol <abhi.dangol@gmail.com>
2021-03-09 15:43:31 -0600

    fixed comment bug

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -1,5 +1,6 @@
 import { getPaginatedBlogs } from "lib/api";
 
+///These are comments
 export default async function getBlogs(req, res) {
   const offset = parseInt(req.query.offset || 0, 10);
   const date = req.query.date || "desc";
